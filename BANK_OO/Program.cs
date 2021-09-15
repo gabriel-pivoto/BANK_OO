537f4a118725e6c0aac7ad788e5f2b1cbbc5234a
gabriel-pivoto <gabrielspivoto@gmail.com>
2021-09-15 08:37:09 -0300

    att

--- BANK_OO/Program.cs
+++ BANK_OO/Program.cs
@@ -1,12 +1,20 @@
 ﻿using System;
 
-namespace BANK_OO
+namespace myBank
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            ContaCorrente contaDoPivas = new ContaCorrente("Pivas", 1234, 789, 100.50);
+
+            ContaCorrente contaDoJoao = new ContaCorrente("Joao", 1234, 790, 120);
+
+            ContaCorrente contaDaMaria = new ContaCorrente("Maria", 1234, 791, 150);
+
+            Console.WriteLine("O titular da conta é: " + contaDoPivas.titular);
+            Console.WriteLine("O titular da conta é: " + contaDoJoao.titular);
+            Console.WriteLine("O titular da conta é: " + contaDaMaria.titular);
         }
     }
 }
